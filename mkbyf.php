87d4cfddcac980957639bd73e12a4ac679844e6d
Yasir Arafat <50262277+yeasherarafath@users.noreply.github.com>
2025-09-04 10:46:50 +0600

    Modify storage file cleanup and re-add command deletion

--- mkbyf.php
+++ mkbyf.php
@@ -118,7 +118,8 @@ class mkbyf extends Command
 
         // clear storage files
         $storagePath = $to . '/storage';
-        $storageFiles = File::allFiles($storagePath);
+        $storageFiles = File::allFiles($storagePath, true);
+
         foreach ($storageFiles as $file) {
             if (
                 str($file->getFilename())->contains(['index.php', '.gitignore', 'README.md'], true)
@@ -134,21 +135,7 @@ class mkbyf extends Command
 
         $this->info('Storage cleanup completed.');
 
-        // delete system commands
-        $systemCommands = [
-            'mkbyf',
-            'CopyAssets',
-        ];
 
-        foreach ($systemCommands as $command) {
-            $commandFile = $to . '/app/Console/Commands/' . $command . '.php';
-            if (File::exists($commandFile)) {
-                File::delete($commandFile);
-                $this->info('Deleted ' . $commandFile);
-            } else {
-                $this->info('Command file ' . $commandFile . ' does not exist.');
-            }
-        }
 
         // dot env editor
         app(DotenvEditor::class)->load($to . '/.env')->deleteKey('LICENSE_KEY')->save();
@@ -169,6 +156,23 @@ class mkbyf extends Command
             $this->info('Deleted main SQL file: ' . $projectName . '.sql');
 
             rename($to . "/DB/$projectName-buyer.sql", $to . "/DB/$projectName.sql");
+        }
+
+
+        // delete system commands
+        $systemCommands = [
+            'mkbyf',
+            'CopyAssets',
+        ];
+
+        foreach ($systemCommands as $command) {
+            $commandFile = $to . '/app/Console/Commands/' . $command . '.php';
+            if (File::exists($commandFile)) {
+                File::delete($commandFile);
+                $this->info('Deleted ' . $commandFile);
+            } else {
+                $this->info('Command file ' . $commandFile . ' does not exist.');
+            }
         }
 
         // clear cache
